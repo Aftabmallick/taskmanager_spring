d72b25e81a2f873acb7c1e4f38f00a28ff0ca005
Aftabmallick <Aftabmallick007@gmail.com>
2023-12-20 12:27:01 +0530

    First Commit

---
+++ src/main/java/org/aftab/taskmanager_/TaskmanagerSpringApplication.java
@@ -0,0 +1,13 @@
+package org.aftab.taskmanager_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/aftab/taskmanager_/TaskmanagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aftab.taskmanager_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
